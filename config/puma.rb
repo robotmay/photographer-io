41171ba9b6f846ca5321845ed8f3726349a4b264
Robert May <robotmay@gmail.com>
2013-05-02 12:50:12 +0100

    Add creator to metadata table

--- config/puma.rb
+++ config/puma.rb
@@ -4,14 +4,3 @@ daemonize       true
 pidfile         "/tmp/web_server.pid"
 bind            "unix:///tmp/web_server.sock"
 stdout_redirect "#{ENV['RAILS_STACK_PATH']}/log/puma.stdout.log", "#{ENV['RAILS_STACK_PATH']}/log/puma.stderr.log"
-
-on_restart do
-  old_pid = '/tmp/web_server.pid.oldbin'
-  if File.exists?(old_pid) && @options[:pid_file] != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
