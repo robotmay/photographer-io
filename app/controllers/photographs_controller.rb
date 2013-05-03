64a0207dde20f32922f88f3ae369ace488851d0a
Robert May <robotmay@gmail.com>
2013-05-03 18:02:41 +0100

    Add a logo

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -2,12 +2,12 @@ class PhotographsController < ApplicationController
   respond_to :html
 
   def index
-    @photographs = Photograph.scoped.page(params[:page])
+    @photographs = Photograph.public.page(params[:page])
     respond_with @photographs
   end
 
   def explore
-    @photographs = Photograph.order("created_at DESC").page(params[:page])
+    @photographs = Photograph.public.order("created_at DESC").page(params[:page])
     respond_with @photographs do |f|
       f.html { render :index }
     end
