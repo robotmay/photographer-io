1074fc24fbe57687ffef17c2ede1192418059d79
Robert May <robotmay@gmail.com>
2013-05-05 00:25:40 +0100

    Don't initialize on precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ Iso::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+  config.assets.initialize_on_precompile = false
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
