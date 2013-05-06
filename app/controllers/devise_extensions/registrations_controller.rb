2bac4d0c75abbf973d8a02400bdcd020b51bc12a
Robert May <robotmay@gmail.com>
2013-05-06 13:11:47 +0100

    Redirect to account page on sign up

--- app/controllers/devise_extensions/registrations_controller.rb
+++ app/controllers/devise_extensions/registrations_controller.rb
@@ -20,6 +20,10 @@ class DeviseExtensions::RegistrationsController < Devise::RegistrationsControlle
   end
 
   protected
+  def after_sign_up_path_for(resource)
+    edit_user_registration_path
+  end
+
   def after_update_path_for(resource)
     edit_user_registration_path
   end
