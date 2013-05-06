b8660c14bef288e235a059031b723b718bed8166
Robert May <robotmay@gmail.com>
2013-05-06 23:07:24 +0100

    Bugfix for location not saving

--- app/controllers/devise_extensions/registrations_controller.rb
+++ app/controllers/devise_extensions/registrations_controller.rb
@@ -31,7 +31,8 @@ class DeviseExtensions::RegistrationsController < Devise::RegistrationsControlle
   private
   def user_params
       params.require(:user).permit(:email, :password, :password_confirmation, 
-                                   :name, :avatar, :retained_avatar, :remove_avatar,
+                                   :name, :location, :avatar, :retained_avatar, 
+                                   :remove_avatar,
                                    :show_location_data, :show_nsfw_content, 
                                    :default_license_id)
   end
