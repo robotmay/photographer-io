a159bab1703ed5ea78a5c2d58d5a49db788a5793
Robert May <robotmay@gmail.com>
2013-05-06 23:57:27 +0100

    Add basis of category browsing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   layout :set_layout
   helper_method :set_title
 
+  before_filter do
+    @categories = Rails.cache.fetch([:categories, :list], expires_in: 5.minutes) do
+      Category.order("name ASC").load
+    end
+  end
+
   protected
   def set_layout
     if devise_controller?
