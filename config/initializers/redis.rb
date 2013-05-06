4c4b16f074f83278c9199c1a3fea6eaafb5ec51a
Robert May <robotmay@gmail.com>
2013-05-06 12:30:22 +0100

    Fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 if ENV['REDIS_PROVIDER']
-  uri = URI.parse(ENV['REDIS_PROVIDER'])
+  uri = URI.parse(ENV[ENV['REDIS_PROVIDER']])
   Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
