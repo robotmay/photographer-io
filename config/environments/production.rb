e8fe264e4a3ceda1756e2efe8b1b30b058056e48
Robert May <robotmay@gmail.com>
2013-05-06 18:59:25 +0100

    Improve logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Iso::Application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
