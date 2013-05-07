a336ee82f33f291595daffa9739ef2669a70651b
Robert May <robotmay@gmail.com>
2013-05-07 19:24:42 +0100

    Up the number of homepage images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   respond_to :html
 
   def home
-    @top_photos = Photograph.landscape.recommended(current_user, 3)
+    @top_photos = Photograph.landscape.recommended(current_user, 5)
     respond_with @top_photos
   end
 end
