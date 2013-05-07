5451896f18732fa89a83b2284b1eb7ebf413dc95
Robert May <robotmay@gmail.com>
2013-05-07 10:15:42 +0100

    Bugfix for seeing other people's collections

--- app/models/collection_photograph.rb
+++ app/models/collection_photograph.rb
@@ -1,4 +1,11 @@
 class CollectionPhotograph < ActiveRecord::Base
   belongs_to :collection
   belongs_to :photograph
+
+  validate :photograph_and_collection_should_belong_to_same_user
+  def photograph_and_collection_should_belong_to_same_user
+    if photograph.user != collection.user
+      errors.add(:base, t("collection_photographs.user_must_match"))
+    end
+  end
 end
