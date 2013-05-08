928bec8104bdefff7c75ff01eda3574296ca387c
Robert May <robotmay@gmail.com>
2013-05-08 11:04:22 +0100

    Add postmark support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Iso::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_KEY'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
