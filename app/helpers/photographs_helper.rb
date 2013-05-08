af5a72687b7a5f8cd9fc870fc378a53665b7ee75
Robert May <robotmay@gmail.com>
2013-05-08 14:47:46 +0100

    Remove redundant field

--- app/helpers/photographs_helper.rb
+++ app/helpers/photographs_helper.rb
@@ -1,7 +1,7 @@
 module PhotographsHelper
   SHORT_METADATA_KEYS = [
     :model, :lens_type, :aperture, :focal_length, :shutter_speed, :iso,
-    :copyright_notice, :rights, :creator, :creator_country, :creator_city,
+    :copyright_notice, :creator, :creator_country, :creator_city,
     :date_time_original
   ]
 
