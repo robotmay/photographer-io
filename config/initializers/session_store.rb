ed4c6c7f3ded1b568ede51223f2100850cc9cb32
Robert May <robotmay@gmail.com>
2013-05-08 03:49:35 +0100

    Improves session expiry, uses Rails RC1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 60.days
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 30.days #NOTE: Do not set higher. Ever. Massive CSRF issues.
