41ad51c5430fe7701e474f5facba41e8af2aecbe
Robert May <robotmay@gmail.com>
2013-05-11 19:50:19 +0100

    Better authorisation failure handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
   protected
   def set_layout
     if devise_controller?
