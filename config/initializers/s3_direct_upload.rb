96ef3b005bef54ab7c6903d6ee69d4384df01662
Robert May <robotmay@gmail.com>
2013-05-13 00:43:30 +0100

    Add convenience connector

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -5,4 +5,10 @@ if ENV['S3_ENABLED']
     c.bucket = ENV['S3_BUCKET']
     c.region = "s3-eu-west-1"
   end
+
+  # Used for debugging
+  $s3_client = S3::Service.new(
+    access_key_id: ENV['S3_KEY'],
+    secret_access_key: ENV['S3_SECRET']
+  )
 end
