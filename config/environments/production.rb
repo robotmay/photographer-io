8534db5e019f4eaa31e12491f81340b3926d6681
Robert May <robotmay@gmail.com>
2013-05-13 13:11:40 +0100

    Switch production logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,8 @@ Iso::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = Logger.new(STDOUT)
+  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  # config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
