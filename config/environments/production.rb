06e5e149e8a8e555a75a737c2f30d8378575839b
Robert May <robotmay@gmail.com>
2013-05-13 13:14:40 +0100

    Update logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Iso::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
   # config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
