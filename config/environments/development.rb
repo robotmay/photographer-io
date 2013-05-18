02a16a6f21e042dfe1194bbb6c38c70893a94d00
Robert May <robotmay@gmail.com>
2013-05-18 12:23:16 +0100

    Improved sign in form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,7 @@ Iso::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: "localhost:5000" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
