df09c6f7bc9a93bcacd8b28964dc851faae836dc
Robert May <robotmay@gmail.com>
2013-05-18 16:46:59 +0100

    More improvements for invitations

--- app/controllers/devise_extensions/invitations_controller.rb
+++ app/controllers/devise_extensions/invitations_controller.rb
@@ -1,6 +1,16 @@
 class DeviseExtensions::InvitationsController < Devise::InvitationsController
+  before_filter :configure_permitted_parameters
+
   protected
   def after_invite_path_for(resource)
     new_user_invitation_path
+  end
+
+  def after_accept_path_for(resource)
+    edit_user_registration_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:accept_invitation) { |u| u.permit(:name, :email, :password, :password_confirmation, :invitation_token) }
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -98,7 +98,7 @@ Devise.setup do |config|
   # You can change invitation_limit column for some users so they can send more
   # or less invitations, even with global invitation_limit = 0
   # Default: nil
-  # config.invitation_limit = 5
+  config.invitation_limit = 10
 
   # The key to be used to check existing users when sending an invitation
   # and the regexp used to test it when validate_on_invite is not set.
