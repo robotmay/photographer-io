1cee0d2e9464af7cf136d382c80af38b9f691d93
Robert May <robotmay@gmail.com>
2013-06-09 01:05:58 +0100

    Updates to workers. Use timeouts to stop dead workers

--- app/workers/metadata_worker.rb
+++ app/workers/metadata_worker.rb
@@ -1,12 +1,15 @@
 class MetadataWorker
   include Sidekiq::Worker
+  include Timeout
   sidekiq_options queue: :photos
 
   def perform(metadata_id)
-    metadata = Metadata.find(metadata_id)
-    metadata.extract_from_photograph
-    metadata.processing = false
-    metadata.save
-    metadata.photograph.trigger_image_processed_push
+    timeout(60) do
+      metadata = Metadata.find(metadata_id)
+      metadata.extract_from_photograph
+      metadata.processing = false
+      metadata.save
+      metadata.photograph.trigger_image_processed_push
+    end
   end
 end
--- app/workers/photo_expansion_worker.rb
+++ app/workers/photo_expansion_worker.rb
@@ -1,27 +1,30 @@
 class PhotoExpansionWorker
   include Sidekiq::Worker
+  include Timeout
   sidekiq_options queue: :photos
 
   def perform(photograph_id)
-    photo = Photograph.find(photograph_id)
-    photo.standard_image = photo.image.thumb("3000x3000>")
-    photo.save!
+    timeout(120) do
+      photo = Photograph.find(photograph_id)
+      photo.standard_image = photo.image.thumb("3000x3000>")
+      photo.save!
 
-    if photo.standard_image.width > photo.standard_image.height
-      photo.homepage_image = photo.standard_image.thumb("2000x").encode(:jpg, "-quality 80")
-      photo.large_image = photo.standard_image.thumb("1500x").encode(:jpg, "-quality 80")
-    elsif photo.standard_image.height > photo.standard_image.width
-      photo.homepage_image = photo.standard_image.thumb("2000x1400#").encode(:jpg, "-quality 80")
-      photo.large_image = photo.standard_image.thumb("x1000").encode(:jpg, "-quality 80")
-    else
-      photo.homepage_image = photo.standard_image.thumb("2000x1400#").encode(:jpg, "-quality 80")
-      photo.large_image = photo.standard_image.thumb("1500x1500>").encode(:jpg, "-quality 80")
+      if photo.standard_image.width > photo.standard_image.height
+        photo.homepage_image = photo.standard_image.thumb("2000x").encode(:jpg, "-quality 80")
+        photo.large_image = photo.standard_image.thumb("1500x").encode(:jpg, "-quality 80")
+      elsif photo.standard_image.height > photo.standard_image.width
+        photo.homepage_image = photo.standard_image.thumb("2000x1400#").encode(:jpg, "-quality 80")
+        photo.large_image = photo.standard_image.thumb("x1000").encode(:jpg, "-quality 80")
+      else
+        photo.homepage_image = photo.standard_image.thumb("2000x1400#").encode(:jpg, "-quality 80")
+        photo.large_image = photo.standard_image.thumb("1500x1500>").encode(:jpg, "-quality 80")
+      end
+
+      photo.thumbnail_image = photo.standard_image.thumb("500x500>").encode(:jpg, "-quality 70")
+
+      photo.processing = false
+      photo.save!
+      photo.trigger_image_processed_push
     end
-
-    photo.thumbnail_image = photo.standard_image.thumb("500x500>").encode(:jpg, "-quality 70")
-
-    photo.processing = false
-    photo.save!
-    photo.trigger_image_processed_push
   end
 end
