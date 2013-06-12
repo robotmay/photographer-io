6e908ec4b41b13fe89b289c9a98c4e788b45165b
Robert May <robotmay@gmail.com>
2013-06-12 13:43:13 +0100

    Revert to one worker for speed

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,9 +1,5 @@
 HireFire::Resource.configure do |config|
   config.dyno(:worker) do
-    HireFire::Macro::Sidekiq.queue(:events, :mailer, :sunspot, :default, :low)
-  end
-
-  config.dyno(:photos) do
-    HireFire::Macro::Sidekiq.queue(:photos)
+    HireFire::Macro::Sidekiq.queue(:events, :mailer, :sunspot, :photos, :default, :low)
   end
 end
