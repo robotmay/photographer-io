64789f1b38b6d7de5ab28a668456608a2f7205ba
Robert May <robotmay@gmail.com>
2013-06-12 23:26:51 +0100

    Split image jobs

---
+++ app/workers/image_worker.rb
@@ -0,0 +1,20 @@
+class ImageWorker
+  include Sidekiq::Worker
+  include Timeout
+  sidekiq_options queue: :photos
+
+  def perform(photo_id, source, target, size, encode_opts)
+    timeout(60) do
+      photo = Photograph.find(photo_id)
+      source = photo.send(source)
+      if source.present?
+        image = source.thumb(size).encode(:jpg, encode_opts)
+        photo.send("#{target}=", image)
+        photo.save!
+        photo.complete_image_processing
+      else
+        raise "Source doesn't exist yet"
+      end
+    end
+  end
+end
