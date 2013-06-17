0c42da9ae6646af6def6d4fcc35d06aeef26768d
Robert May <robotmay@gmail.com>
2013-06-17 23:14:25 +0100

    Improved deploy locations

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,15 +14,15 @@ set :use_sudo, false
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/app"
 set :deploy_via, :remote_cache
 
 set :bundle_without, [:development, :test, :acceptance]
 
 set :foreman_sudo, sudo
-set :foreman_upstart_path, '/etc/init'
+set :foreman_upstart_path, '/etc/init/app'
 set :foreman_options, {
-  app: application,
+  app: "app/#{application}",
   log: "#{shared_path}/log",
   user: user
 }
@@ -36,7 +36,7 @@ role :app
 role :db
 
 server "pio-web-1", :web, :app, :db, primary: true
-#server "pio-web-2", :web, :app
+server "pio-web-2", :web, :app
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -60,45 +60,41 @@ namespace :deploy do
   end
 end
 
-after 'deploy:update_code', :'foreman:export'
-
 before 'dotenv:symlink', :upload_env_vars
 task :upload_env_vars do
-  upload(".env.#{rails_env}", "#{shared_path}/.env", :via => :scp)
+  upload(".env.#{rails_env}", "#{shared_path}/.env", via: :scp)
+end
+
+task :upload_certs do
+  %w{bundle.pem server.crt server.key}.each do |file|
+    upload(file, "#{shared_path}/#{file}", via: :scp)
+  end
 end
 
 namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export, roles: :app do
-    cmd = 'bundle exec foreman'
+    cmd = 'foreman'
     run "if [[ -d #{foreman_upstart_path} ]]; then #{sudo} mkdir -p #{foreman_upstart_path}; fi"
-    run "cd #{current_path} && #{sudo} #{cmd} export upstart #{foreman_upstart_path} #{format(options)}"
+    run "cd #{current_path} && #{cmd} export upstart #{foreman_upstart_path} #{format(foreman_options)}"
   end
 
   desc "Start the application services"
   task :start, roles: :app do
-    run "#{sudo} service #{options[:app]} start"
+    run "#{sudo} service #{foreman_options[:app]} start"
   end
 
   desc "Stop the application services"
   task :stop, roles: :app do
-    run "#{sudo} service #{options[:app]} stop"
+    run "#{sudo} service #{foreman_options[:app]} stop"
   end
 
   desc "Restart the application services"
   task :restart, roles: :app do
-    run "#{sudo} service #{options[:app]} start || #{sudo} service #{options[:app]}  restart"
+    run "#{sudo} service #{foreman_options[:app]} start || #{sudo} service #{foreman_options[:app]}  restart"
   end
 
-  def options
-    {
-      app: application,
-      log: "#{shared_path}/log",
-      user: user
-    }.merge foreman_options
-  end
-
-  def format opts
+  def format(opts)
     opts.map { |opt, value| "--#{opt}=#{value}" }.join " "
   end
 end
