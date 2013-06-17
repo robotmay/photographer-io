385938a0866c862a1b25285218abf66c005e335c
Robert May <robotmay@gmail.com>
2013-06-17 23:14:25 +0100

    Improved deploy locations

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 environment     $RAILS_ENV
 threads         16, 16
 daemonize       true
-pidfile         "/var/run/web_server.pid"
-bind            "unix:///var/run/web_server.sock"
+pidfile         "/var/run/app/web_server.pid"
+bind            "unix:///var/run/app/web_server.sock"
 
 # Add a worker per CPU core
 workers         %x{grep -c processor /proc/cpuinfo}.strip
