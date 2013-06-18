9470c59a639fd6e15b6f5ec28aadfb58e4de95b4
Robert May <robotmay@gmail.com>
2013-06-18 10:36:21 +0100

    Don't daemonize

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 environment     $RAILS_ENV
 threads         16, 16
-daemonize       true
+#daemonize       true
 pidfile         "/var/run/app/web_server.pid"
 bind            "unix:///var/run/app/web_server.sock"
 
 # Add a worker per CPU core
-#workers         %x{grep -c processor /proc/cpuinfo}.strip
+workers         %x{grep -c processor /proc/cpuinfo}.strip
