d6d1a1a9860607b6f45def8cdf5d1d7e9a4c3802
Robert May <robotmay@gmail.com>
2013-06-19 14:39:13 +0100

    More deploy fixes

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 environment     $RAILS_ENV
 threads         16, 16
 pidfile         "/var/run/app/web_server.pid"
-state           "/var/run/app/web_server.state"
+state_path      "/var/run/app/web_server.state"
 bind            "unix:///var/run/app/web_server.sock"
 control         "unix:///var/run/app/web_control.sock"
 
