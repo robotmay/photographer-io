c5aa502db0149e9f7e9cb4043804be84379394d5
Robert May <robotmay@gmail.com>
2013-06-19 14:32:44 +0100

    More deploy fixes

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,5 @@
 environment     $RAILS_ENV
 threads         16, 16
-#daemonize       true
 pidfile         "/var/run/app/web_server.pid"
 state           "/var/run/app/web_server.state"
 bind            "unix:///var/run/app/web_server.sock"
@@ -8,3 +7,5 @@ control         "unix:///var/run/app/web_control.sock"
 
 # Add a worker per CPU core
 workers         %x{grep -c processor /proc/cpuinfo}.strip
+
+activate_control_app
