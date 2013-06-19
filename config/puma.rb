c940a7473081f98ec3da61401a244e3df274d946
Robert May <robotmay@gmail.com>
2013-06-19 14:23:23 +0100

    Use current protocol for image requests

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,9 @@ environment     $RAILS_ENV
 threads         16, 16
 #daemonize       true
 pidfile         "/var/run/app/web_server.pid"
+state           "/var/run/app/web_server.state"
 bind            "unix:///var/run/app/web_server.sock"
+control         "unix:///var/run/app/web_control.sock"
 
 # Add a worker per CPU core
 workers         %x{grep -c processor /proc/cpuinfo}.strip
