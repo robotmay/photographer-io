c964fb85cef2786d270d85b0c375e7abf8e902c3
Robert May <robotmay@gmail.com>
2013-06-19 14:43:02 +0100

    More deploy fixes

--- config/puma.rb
+++ config/puma.rb
@@ -3,9 +3,8 @@ threads         16, 16
 pidfile         "/var/run/app/web_server.pid"
 state_path      "/var/run/app/web_server.state"
 bind            "unix:///var/run/app/web_server.sock"
-control         "unix:///var/run/app/web_control.sock"
 
 # Add a worker per CPU core
 workers         %x{grep -c processor /proc/cpuinfo}.strip
 
-activate_control_app
+activate_control_app "unix:///var/run/app/web_control.sock"
