dfa28607517b95dd7e422d26956d2861c99d495e
Robert May <robotmay@gmail.com>
2013-06-21 14:56:48 +0100

    Fix for pool size

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,3 +89,11 @@ task :upload_certs do
     upload("certs/#{file}", "#{certs_path}/#{file}", via: :scp)
   end
 end
+
+namespace :sake do
+  desc "Run a task on a remote server."
+  # run like: cap staging rake:invoke task=a_certain_task  
+  task :invoke do
+    run("cd #{deploy_to}/current && bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}")
+  end
+end
