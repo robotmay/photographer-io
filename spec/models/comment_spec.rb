540c3fc84bc02406d209b50fb1f7cd12b208a0cb
Robert May <robotmay@gmail.com>
2013-07-07 11:55:14 +0100

    Starting to improve specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:comment_thread) }
+  it { should have_many(:notifications) }
 end
--- spec/models/photograph_spec.rb
+++ spec/models/photograph_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 
 describe Photograph do
-  it { should belong_to(:user) }
-  it { should have_one(:metadata) }
-  it { should have_many(:collection_photographs) }
-  it { should have_many(:collections).through(:collection_photographs) }
-  it { should belong_to(:license) }
-  it { should have_many(:recommendations) }
-  it { should belong_to(:category) }
-  it { should have_many(:favourites) }
-  it { should have_many(:favourited_by_users).through(:favourites) }
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should have_one(:metadata) }
+    it { should have_many(:collection_photographs) }
+    it { should have_many(:collections).through(:collection_photographs) }
+    it { should belong_to(:license) }
+    it { should have_many(:recommendations) }
+    it { should belong_to(:category) }
+    it { should have_many(:favourites) }
+    it { should have_many(:favourited_by_users).through(:favourites) }
+    it { should have_many(:comment_threads) }
+    it { should have_many(:comments).through(:comment_threads) }
+  end
 
-  [:user_id, :image].each do |attr|
-    it { should validate_presence_of(attr) }
+  describe "validations" do
+    let(:user) { User.make }
+
+    before(:all) do
+      subject.stub(:user).and_return(user)
+    end
+
+    [:user_id, :image_uid].each do |attr|
+      it { should validate_presence_of(attr) }
+    end
   end
 end
