c7348f4af8332b715d550017895d5507e1f0853f
Robert May <robotmay@gmail.com>
2013-07-15 09:46:52 +0100

    Adding basic request specs for authentication

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,7 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 30.days #NOTE: Do not set higher. Ever. Massive CSRF issues.
+
+if Rails.env.production?
+  Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 30.days #NOTE: Do not set higher. Ever. Massive CSRF issues.
+else
+  Rails.application.config.session_store :cookie_store, key: '_pio_session'
+end
