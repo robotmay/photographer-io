5f9c2097ecd0ebf302018c31e04b3382939fc92f
Robert May <robotmay@gmail.com>
2013-07-15 10:13:57 +0100

    Bugfixes for testing environment

--- config/initializers/gabba.rb
+++ config/initializers/gabba.rb
@@ -1,1 +1,3 @@
-$gabba = Gabba::Gabba.new(ENV['GOOGLE_ANALYTICS_ID'], ENV['DOMAIN'])
+if ENV['GOOGLE_ANALYTICS_ID'].present?
+  $gabba = Gabba::Gabba.new(ENV['GOOGLE_ANALYTICS_ID'], ENV['DOMAIN'])
+end
