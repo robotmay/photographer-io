877fccac8f43a470bbd8a4d5705b694944bf5217
Robert May <robotmay@gmail.com>
2013-07-28 15:34:58 +0100

    Add some controller specs for Collections

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
 describe CollectionsController do
+  let(:user) { User.make! }
+  before { sign_in user }
 
+  describe "GET show" do
+    let(:collection) { Collection.make(visible: visibility) }
+    let(:photographs) { [Photograph.make] }
+
+    before do
+      collection.stub(:id) { 1 }
+      collection.stub_chain(:photographs, :page) { photographs }
+      Collection.stub(:find) { collection }
+    end
+
+    context "public collection" do
+      let(:visibility) { true }
+
+      it "renders the view" do
+        get :show, id: 1
+        response.should render_template(:show)
+      end
+
+      it "assigns @collection" do
+        get :show, id: 1
+        assigns(:collection).should eq(collection)
+      end
+
+      it "assigns @photographs" do
+        get :show, id: 1
+        assigns(:photographs).should eq(photographs)
+      end
+    end
+
+    context "private collection" do
+      let(:visibility) { false }
+
+      it "raises an error" do
+        get :show, id: 1
+        response.should redirect_to(root_url)
+      end
+    end
+  end
 end
