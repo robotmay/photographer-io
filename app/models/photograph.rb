434b7f38a5606d77afebd9a54237710de3c82480
Robert May <robotmay@gmail.com>
2013-08-23 16:53:22 +0100

    Bugfix

--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -356,18 +356,13 @@ class Photograph < ActiveRecord::Base
     #
     # @return [Array]
     def recommended(opts = {})
-      all_photo_ids = if opts[:n].present? && opts[:n] > 0
+      photo_ids = if opts[:n].present? && opts[:n] > 0
         Photograph.rankings.revrange(0,(opts[:n] - 1))
       else
-        Photograph.rankings.members.reverse
+        ids = Photograph.rankings.members.reverse
+        Kaminari.paginate_array(ids).page(opts[:page]).per(Photograph.default_per_page)
       end
       
-      photo_ids = if opts[:page].present?
-        Kaminari.paginate_array(all_photo_ids).page(opts[:page]).per(Photograph.default_per_page)
-      else
-        all_photo_ids
-      end
-
       photographs = view_for(nil).includes(:metadata).where(id: photo_ids).group_by(&:id)
       sorted_photographs = photo_ids.map { |id| photographs[id.to_i] }.compact.map(&:first)
       Kaminari.paginate_array(sorted_photographs).page(opts[:page]).per(Photograph.default_per_page)
